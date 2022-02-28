d6fd1ffdf01c3b998d6474c7cf91a482398f3a2a
мансур зурабов <asl.zurabov@gmail.com>
2022-02-28 14:33:48 +0300

    initial commit

---
+++ test/test_ax1.py
@@ -0,0 +1,11 @@
+import pytest
+def test_ex():
+    assert 1 == 1
+def test_ex2():
+    assert 1 == 1
+def test_ex2():
+    assert 1 == 2
+def test_ex2():
+    assert 1 == 1
+def test_ex2():
+    assert 1 == 1
